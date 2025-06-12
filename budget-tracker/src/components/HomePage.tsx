1054ba409eb5264116a7d047d7b346665fe1ab3e
srcNathan <nathanialsw@protonmail.com>
2025-06-11 20:42:21 -0400

    Messing with colors

--- budget-tracker/src/components/HomePage.tsx
+++ budget-tracker/src/components/HomePage.tsx
@@ -2,6 +2,7 @@ export default function HomePage() {
   return (
     <div className="home-container">
       <h1>Budget Tracker</h1>
+      <h3>Take Control of Your Finances and Save More</h3>
       <button>Start</button>
     </div>
   );
