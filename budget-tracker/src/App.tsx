d58b372a57a76b467abd66962b2fd0ca8de0cbd9
srcNathan <nathanialsw@protonmail.com>
2025-06-12 17:25:50 -0400

    Splitting css files

--- budget-tracker/src/App.tsx
+++ budget-tracker/src/App.tsx
@@ -1,7 +1,5 @@
-import React from "react";
 import HomePage from "./components/HomePage";
 import BackgroundCanvas from "./components/BackgroundCanvas";
-import "./index.css";
 
 export default function App() {
   return (
--- budget-tracker/src/components/HomePage.tsx
+++ budget-tracker/src/components/HomePage.tsx
@@ -1,3 +1,5 @@
+import "../styles/homepage.css";
+
 export default function HomePage() {
   return (
     <div className="home-container">
