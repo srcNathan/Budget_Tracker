75bca83a1148a06b38e7e30047d492d3953cd2e0
srcNathan <nathanialsw@protonmail.com>
2025-06-12 17:14:27 -0400

    Adding a card to make the text easier to read

--- budget-tracker/src/components/HomePage.tsx
+++ budget-tracker/src/components/HomePage.tsx
@@ -1,9 +1,11 @@
 export default function HomePage() {
   return (
     <div className="home-container">
-      <h1>Budget Tracker</h1>
-      <h3>Take Control of Your Finances and Save More</h3>
-      <button>Start</button>
+      <div className="home-card">
+        <h1>Budget Tracker</h1>
+        <h3>Take Control of Your Finances and Save More</h3>
+        <button>Start</button>
+      </div>
     </div>
   );
 }
